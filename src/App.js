6f4eaa90c15555c3167036e3a6c0064124a8435a
uccwal <uccwallll@gmail.com>
2023-08-01 13:53:52 +0900

    Myapp Main

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    MyApp
+                    MyApp Main
                 </Typography>
             </Toolbar>
         </AppBar>
